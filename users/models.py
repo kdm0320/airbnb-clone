eef2ebd08a4e73940bfed8b86eb881cc5dab8745
Dogmin <kdm3578@gmail.com>
2021-07-30 14:43:39 +0900

    Changed ModelForm

--- users/models.py
+++ users/models.py
@@ -3,40 +3,43 @@ from django.db import models
 from django.db.models.fields import DateField
 
 
-
 class User(AbstractUser):
-    """ Custom User Model"""
+    """Custom User Model"""
 
-    GENDER_MALE= "male"
-    GENDER_FEMALE= "female"
-    GENDER_OTHER= "other"
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
 
-    GENDER_CHOICES= (
+    GENDER_CHOICES = (
         (GENDER_MALE, "Male"),
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
-
     )
 
     LANGUAGE_ENGLISH = "english"
     LANGUAGE_KOREAN = "korean"
-    LANGUAGE_CHOICES= (
+    LANGUAGE_CHOICES = (
         (LANGUAGE_ENGLISH, "English"),
         (LANGUAGE_KOREAN, "Korean"),
     )
 
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
-    CURRENCY_CHOICES= (
+    CURRENCY_CHOICES = (
         (CURRENCY_USD, "USD"),
         (CURRENCY_KRW, "KRW"),
     )
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True) #single line
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, blank=True
+    )  # single line
     bio = models.TextField(blank=True)
-    birthday = models.DateField(null=True,blank=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES,max_length=6,blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES,max_length=3,blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=6, blank=True, default=LANGUAGE_KOREAN
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
+    )
     superhost = models.BooleanField(default=False)
-
